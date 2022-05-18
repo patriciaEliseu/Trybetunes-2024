f7fe3046872c2b59d45e414d738a906218f3ded7
Patricia Eliseu <patyeliseu4@gmail.com>
2022-05-18 19:20:41 -0300

    5. Crie o formulário para pesquisar artistas

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,11 +1,57 @@
 import React, { Component } from 'react';
 import Header from '../Components/Header';
+// import Loading from '../Components/Loading';
 
 class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      inputNameSearch: '',
+      buttonTrueSearch: true,
+    };
+  }
+
+  onInput = ({ target }) => {
+    const { name, value } = target;
+    this.setState({ [name]: value }, () => {
+      const { inputNameSearch } = this.state;
+      const numberTwo = 2;
+      if (inputNameSearch.length >= numberTwo) {
+        this.setState({ buttonTrueSearch: false });
+      } else {
+        this.setState({ buttonTrueSearch: true });
+      }
+    });
+  }
+
   render() {
+    const { inputNameSearch, buttonTrueSearch } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        {/*   {
+          carregaTelaSearch ? <Loading /> : ( */}
+        <form>
+          <label htmlFor="text">
+            <input
+              name="inputNameSearch"
+              type="text"
+              data-testid="search-artist-input"
+              value={ inputNameSearch }
+              onChange={ this.onInput }
+            />
+            <button
+              data-testid="search-artist-button"
+              type="button"
+              disabled={ buttonTrueSearch }
+              onClick={ this.createClickSearch }
+            >
+              Pesquisar
+            </button>
+          </label>
+        </form>
+        {/*    )
+        } */}
       </div>
     );
   }
