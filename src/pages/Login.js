2ce61b62fdf2e6faef95ef27bd43924ae14b69f4
Patricia Eliseu <patyeliseu4@gmail.com>
2022-05-18 17:46:30 -0300

    2. Crie um formulário para identificação

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,75 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import Loading from '../Components/Loading';
+import { createUser } from '../services/userAPI';
 
 class Login extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      inputName: '',
+      buttonTrue: true,
+      carregaTela: false,
+    };
+  }
+
+  onInput = ({ target }) => {
+    const { name, value } = target;
+    this.setState({ [name]: value }, () => {
+      const { inputName } = this.state;
+      const numberThree = 3;
+      if (inputName.length >= numberThree) {
+        this.setState({ buttonTrue: false });
+      } else {
+        this.setState({ buttonTrue: true });
+      }
+    });
+  }
+
+  createClick = async () => {
+    const { inputName } = this.state;
+    this.setState({
+      carregaTela: true,
+      redir: false,
+    });
+    await createUser({ name: inputName });
+    this.setState({
+      carregaTela: false,
+      redir: true,
+    });
+  }
+
   render() {
+    const { inputName, buttonTrue, carregaTela, redir } = this.state;
     return (
       <div data-testid="page-login">
-        <p>login</p>
+        {
+          carregaTela ? <Loading /> : (
+            <form>
+              <label htmlFor="name">
+                <input
+                  name="inputName"
+                  data-testid="login-name-input"
+                  type="text"
+                  value={ inputName }
+                  onChange={ this.onInput }
+                />
+                <button
+                  data-testid="login-submit-button"
+                  type="button"
+                  disabled={ buttonTrue }
+                  onClick={ this.createClick }
+                >
+                  Entrar
+                </button>
+              </label>
+            </form>
+          )
+        }
+        {
+          redir && <Redirect to="/search" />
+        }
       </div>
     );
   }
