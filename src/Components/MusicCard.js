90d6da1c3c3e65213896b53042b6d24fe140b168
Patricia Eliseu <patyeliseu4@gmail.com>
2022-05-20 19:41:42 -0300

    8. Crie o mecanismo para adicionar músicas na lista de músicas favoritas

--- src/Components/MusicCard.js
+++ src/Components/MusicCard.js
@@ -1,84 +1,81 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-// import Loading from './Loading';
-// import { addSong } from '../services/favoriteSongsAPI';
+import Loading from './Loading';
+import { addSong, getFavoriteSongs } from '../services/favoriteSongsAPI';
+// import { getMusics } from '../services/musicsAPI';
 
 // console.log('sereieu', getFavoriteSongs());
 
 class MusicCard extends Component {
-  /*  constructor() {
+  constructor() {
     super();
     this.state = {
-      favorita: false,
       carregaTela: false,
-    };
- */
-  /* checkOnChange = ({ target }) => {
-      // console.log('judas', target);
-      const { musica } = this.props;
-      this.setState({
-        favorita: target.checked,
-      }, async () => {
-        const { favorita } = this.state;
-        // console.log(favorita);
-        if (favorita === true) {
-          this.setState({
-            carregaTela: true,
-          });
-          await addSong(musica);
-          console.log('sim', addSong(musica));
-          this.setState({
-            carregaTela: false,
-          });
-        }
-      });
-    };
+      listaFavorita: [],
 
-    componentDidMount(() => {
-      this.setState({
-        favorita: checked.some((musica) => musica.trackId === trackId),
-      });
+    };
+  }
+
+  async componentDidMount() {
+    await this.newListaFavoritas();
+  }
+
+  newListaFavoritas = async () => {
+    const listaFav = await getFavoriteSongs();
+    this.setState({
+      listaFavorita: listaFav,
     });
   }
- */
+
+  checkOnChange = async () => {
+    this.setState({
+      carregaTela: true,
+    });
+    const { musica } = this.props;
+    await addSong(musica);
+    await this.newListaFavoritas();
+    this.setState({
+      carregaTela: false,
+    });
+  }
+
   render() {
-    const { resumo, nome/*  trackId */ } = this.props;
-    // const { carregaTela, favorita } = this.state;
+    const { resumo, nome, trackId } = this.props;
+    const { carregaTela, listaFavorita } = this.state;
 
     return (
       <div>
-        <p>{ nome }</p>
+        <p>{nome}</p>
         <audio data-testid="audio-component" src={ resumo } controls>
           <track kind="captions" />
           O seu navegador não suporta o elemento
           <code>audio</code>
-          .
         </audio>
-        {/*  {
+        {
           carregaTela ? <Loading /> : (
-            <label htmlFor={ trackId }>
+            <label htmlFor="input">
               Favorita
               <input
                 name="inputFavorita"
                 id={ trackId }
                 data-testid={ `checkbox-music-${trackId}` }
                 type="checkbox"
-                checked={ favorita }
-                onChange={ this.checkOnChange() }
+                checked={ listaFavorita.some((disco) => disco.trackId === trackId) }
+                onChange={ this.checkOnChange }
               />
             </label>
           )
-        } */}
+        }
       </div>
     );
   }
 }
 
 MusicCard.propTypes = {
+  musica: PropTypes.objectOf().isRequired,
   resumo: PropTypes.string.isRequired,
   nome: PropTypes.string.isRequired,
-  // trackId: PropTypes.number.isRequired,
-  // musica: PropTypes.string.isRequired,
+  trackId: PropTypes.number.isRequired,
 };
 
 export default MusicCard;
